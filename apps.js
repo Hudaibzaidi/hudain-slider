dc74f7e941f5796e4fc050fdf9ed2a4e9f0d5854
Hudaibzaidi <117076704+Hudaibzaidi@users.noreply.github.com>
2022-12-05 16:47:54 +0500

    hudain slider
    images

---
+++ apps.js
@@ -0,0 +1,28 @@
+var room =[
+    "carimage.jpg",
+    "bikeimage1.jpg",
+    "lionimage.jpg",
+]
+var image = 0;
+var bulb = room.length-1;
+var laptop = 0;
+
+var button1 =document.getElementById("button1")
+function Next(){
+    tere =document. getElementById("car")
+    laptop++;
+    if(laptop >= bulb){
+        laptop = 2
+    }
+    tere.src = room[laptop]
+}
+
+var button2 =document.getElementById("button2")
+function Previous(){
+    tere =document. getElementById("car")
+    laptop--;
+    if(laptop <= image){
+        laptop = 0
+    }
+    tere.src = room[laptop]
+}
